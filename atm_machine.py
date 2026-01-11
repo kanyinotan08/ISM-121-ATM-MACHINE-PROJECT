b44d34a863998c05d5eb115128ac0cc4725278f7
kanyinotan08 <kanyinoduntan08@gmail.com>
2026-01-12 00:13:34 +0100

    initial commit

---
+++ atm_machine.py
@@ -0,0 +1,62 @@
+ccount_number = ""
+pin = ""
+balance = 0
+
+    # Create account
+account_number = input("Enter new account number: ")
+pin = input("Set your PIN: ")
+print("Account created successfully!\n")
+
+    # If user wants to continue
+continue_using = "YES"
+while continue_using == "YES":
+ 
+        # Deposit
+    entered_pin = input("Enter PIN to deposit: ")
+    if entered_pin == pin:
+        amount = int(input("Enter amount to deposit: "))
+        balance += amount
+        print("Deposit successful. Balance:", balance)
+    else:
+        print("Wrong PIN.")
+
+    continue_using = input("\nContinue? (YES/NO): ")
+    if continue_using != "YES":
+        break
+
+        # Check balance
+        entered_pin = input("\nEnter PIN to check balance: ")
+        if entered_pin == pin:
+            print("Your balance is:", balance)
+        else:
+            print("Wrong PIN.")
+
+        continue_using = input ("\nContinue? (YES/NO): ")
+        if continue_using != "YES":
+            break
+
+        # Withdraw
+    entered_pin = input("\nEnter PIN to withdraw: ")
+    if entered_pin == pin:
+        amount = int(input("Enter amount to withdraw: "))
+        if amount <= balance:
+            balance -= amount
+            print("Withdrawal successful. Balance:", balance)
+        else:
+            print("Insufficient funds.")
+    else:
+        print("Wrong PIN.")
+
+        continue_using = input("\nContinue? (YES/NO): ")
+        if continue_using != "YES":
+            break
+
+        # Update PIN
+    entered_pin = input("\nEnter old PIN to update: ")
+    if entered_pin == pin:
+        pin = input("Enter new PIN: ")
+        print("PIN updated successfully!")
+    else:
+        print("Wrong PIN.")
+
+        continue_using = input("\nContinue? (YES/NO): ")
